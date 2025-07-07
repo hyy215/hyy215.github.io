16d5ea913c963a3b05a35d5ef5d9dbcd704f714f
huangyanyan <hyy215215.163.com>
2025-07-07 10:42:47 +0800

    上传React文章

--- blog/.vuepress/sidebar.ts
+++ blog/.vuepress/sidebar.ts
@@ -36,7 +36,16 @@ export default sidebar({
       icon: "react",
       prefix: "react/",
       link: "react/",
-      children: [{
+      children: [
+        {
+          text: "React 源码解析（一） —— Virtual DOM",
+          icon: "lightbulb-on-outline",
+          link: "react-virtual-dom.html"
+        },{
+          text: "React Redux 使用小记",
+          icon: "lead-pencil",
+          link: "react-redux.html"
+        },{
         text: "React Scheduler",
         link: "react-scheduler.html"
       }],
--- blog/react/README.md
+++ blog/react/README.md
@@ -5,4 +5,6 @@ category:
   - React
 ---
 
+* [React 源码解析（一） —— Virtual DOM](./react-virtual-dom.html)
 * [React Scheduler](./react-scheduler.html)
+* [React Redux 使用小记](./react-redux.html)
